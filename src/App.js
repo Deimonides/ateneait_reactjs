1ee76ab8bd8537b16fb16f3c591d811ea487ab01
Deimonides <deimonides@gmail.com>
2022-02-09 20:02:01 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Bienvenidos al eCommerce de AteneaIT</h1>
+      <h2>Ir al catálogo</h2>
+      
+
+    </div>
+  );
+}
+
+export default App;
