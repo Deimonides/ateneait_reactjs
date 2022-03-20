049b51cf37fffce8f6514bd35921f08cad18b973
Deimonides <deimonides@gmail.com>
2022-03-19 23:51:47 -0300

    Desafio 6 Parte 2- Usando Firestore - a Netlify III

--- src/components/Context/CartContext.jsx
+++ src/components/Context/CartContext.jsx
@@ -14,7 +14,7 @@ export function CartContextProvider({ children }) {
            /*  console.log('Es indefinido --> Item nuevo en carrito'); */
             const itemNuevo = { ...item, itemCant }; // generar el objeto del item incluyéndole la cantidad
             setItemsInCart([...itemsInCart, itemNuevo]); // Se agrega item nuevo
-            console.log('array: ' , [...itemsInCart]);
+            /* console.log('array: ' , [...itemsInCart]); */
         } else {
             /* console.log('No es indefinido --> Item existe en carrito'); */ // el producto SI existe en el carrito
             let itemRepetidoIndex = itemsInCart.findIndex( elemento => elemento.id === item.id )
@@ -38,20 +38,20 @@ export function CartContextProvider({ children }) {
     function contarItems () { 
         let array =  [...itemsInCart]
         let itemsTotales = 0;
-        let precioTotal = 0; 
+        /* let precioTotal = 0; */ 
         
         /* let total = itemsInCart.map ( element => { element.itemCant += total }  ) */
         /* itemsInCart.forEach ( function (element)  {total += element.itemCant} ) */
         /* itemsInCart.map(el => el.itemCant += total ).reduce((total, val) => total + val.itemCant, 0) */
         
-        console.log('array: ' , itemsInCart)
+        /* console.log('array: ' , itemsInCart) */
         /* itemsTotales = array.reduce((acc, el) => acc + el.itemCant, 0) */
         setTotales(array.reduce((acc, el) => acc + el.itemCant, 0))
-        console.log('itemsTotales: ' , itemsTotales)
+        /* console.log('itemsTotales: ' , itemsTotales) */
         /* precioTotal = array.reduce((acc, el) => acc + (el.precio * el.itemCant) , 0)
         console.log('precioTotal: ' , precioTotal) */
         /* setTotales(itemsTotales) */
-        console.log("totales: " , totales);
+        /* console.log("totales: " , totales); */
         /* total = acc */
         /* cantidadItems = itemsInCart.reduce( (acc, el) => acc + el.itemCant, 0) */
         /* cantidadItems = itemsInCart.map () */
--- src/components/ItemDetail/ItemDetailContainer.jsx
+++ src/components/ItemDetail/ItemDetailContainer.jsx
@@ -8,23 +8,26 @@ function ItemDetailContainer(  ) {
     const [producto, setProducto] = useState([])
 
     useEffect(() => {
-        getItem()
-    }, [id])
+        
     
-    const getItem = () => {
-        const getItemPromise = new Promise( (resolve , reject) => {
-            const itemSeleccionado = JsonProductos.find( item => {
-                return item.id === id
-            })
-            resolve(itemSeleccionado)
-        })  
-        getItemPromise.then(
-            dato => {
-                setProducto(dato)
-                /* console.log(dato) */
-            }
-        )
-    }
+    
+        function getItem() {
+            const getItemPromise = new Promise( (resolve) => {
+                const itemSeleccionado = JsonProductos.find( item => {
+                    return item.id === id
+                })
+                resolve(itemSeleccionado)
+            })  
+            getItemPromise.then(
+                dato => {
+                    setProducto(dato)
+                    /* console.log(dato) */
+                }
+            )
+        }
+        getItem()
+    }, [])
+    
     return (
         <div>            
             <ItemDetail item={producto}
